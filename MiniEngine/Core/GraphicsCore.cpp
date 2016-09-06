d12e5da7dba6671c7b2976d4c3459a2ee9d6e82e
Matt Lee <matt.lee@gmail.com>
2016-09-05 17:33:03 -0700

    Adjust DXGI adapter detection to prefer non-Intel GPUs when more than one hardware GPU is detected.

--- MiniEngine/Core/GraphicsCore.cpp
+++ MiniEngine/Core/GraphicsCore.cpp
@@ -335,6 +335,7 @@ void Graphics::Initialize(void)
 
 	// Create the D3D graphics device
 	Microsoft::WRL::ComPtr<IDXGIAdapter1> pAdapter;
+    Microsoft::WRL::ComPtr<IDXGIAdapter1> pSelectedAdapter;
 
 	static const bool bUseWarpDriver = false;
 
@@ -349,17 +350,31 @@ void Graphics::Initialize(void)
 			if (desc.Flags & DXGI_ADAPTER_FLAG_SOFTWARE)
 				continue;
 
-			if (desc.DedicatedVideoMemory > MaxSize && SUCCEEDED(D3D12CreateDevice(pAdapter.Get(), D3D_FEATURE_LEVEL_11_0, MY_IID_PPV_ARGS(&pDevice))))
-			{
-				pAdapter->GetDesc1(&desc);
-				Utility::Printf(L"D3D12-capable hardware found:  %s (%u MB)\n", desc.Description, desc.DedicatedVideoMemory >> 20);
-				MaxSize = desc.DedicatedVideoMemory;
-			}
-		}
+            if (pSelectedAdapter == nullptr)
+            {
+                pSelectedAdapter = pAdapter;
+            }
+            else if (desc.VendorId != 0x8086)
+            {
+                pSelectedAdapter = pAdapter;
+            }
 
-		if (MaxSize > 0)
-			g_Device = pDevice.Detach();
+            if (desc.DedicatedVideoMemory > MaxSize && SUCCEEDED(D3D12CreateDevice(pAdapter.Get(), D3D_FEATURE_LEVEL_11_0, MY_IID_PPV_ARGS(&pDevice))))
+            {
+                pAdapter->GetDesc1(&desc);
+                Utility::Printf(L"D3D12-capable hardware found:  %s (%u MB)\n", desc.Description, desc.DedicatedVideoMemory >> 20);
+                MaxSize = desc.DedicatedVideoMemory;
+            }
+        }
 	}
+
+    if (pSelectedAdapter != nullptr && SUCCEEDED(D3D12CreateDevice(pSelectedAdapter.Get(), D3D_FEATURE_LEVEL_11_0, MY_IID_PPV_ARGS(&pDevice))))
+    {
+        DXGI_ADAPTER_DESC1 desc;
+        pSelectedAdapter->GetDesc1(&desc);
+        Utility::Printf(L"D3D12-capable hardware found:  %s (%u MB)\n", desc.Description, desc.DedicatedVideoMemory >> 20);
+        g_Device = pDevice.Detach();
+    }
 
 	if (g_Device == nullptr)
 	{
