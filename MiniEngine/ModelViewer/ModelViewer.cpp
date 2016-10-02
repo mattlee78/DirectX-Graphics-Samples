affe9349edb713a1f4f079afe94adb82abfbbc32
Matt Lee <matt.lee@gmail.com>
2016-10-01 21:47:55 -0700

    Added tree model

--- MiniEngine/ModelViewer/ModelViewer.cpp
+++ MiniEngine/ModelViewer/ModelViewer.cpp
@@ -165,7 +165,7 @@ void ModelViewer::Startup( void )
     m_InstanceMap[0] = pMI;
 
     pMI = new ModelInstance();
-    pMI->InitializeModel("Models/cube.bmesh");
+    pMI->InitializeModel("Models/BeechAmerican_A_LOD0.bmesh");
     m_InstanceMap[1] = pMI;
 
 	CreateParticleEffects();
