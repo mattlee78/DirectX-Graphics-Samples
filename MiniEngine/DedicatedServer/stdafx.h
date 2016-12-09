73e2af42ac9be395785ed8ee8091d36f80c9dd4e
Matt Lee <matt.lee@gmail.com>
2016-12-09 13:07:11 -0800

    Dedicated server skeleton project; not yet quite working.

---
+++ MiniEngine/DedicatedServer/stdafx.h
@@ -0,0 +1,11 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#include "..\Core\pch.h"
+
+#include "SystemTime.h"
+#include "NetworkLayer.h"
