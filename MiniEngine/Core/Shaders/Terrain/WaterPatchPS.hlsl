0ffc10ff94029b1028843281f18d678956ae8049
Matt Lee <matt.lee@gmail.com>
2017-04-05 15:55:32 -0700

    Water, v1

--- MiniEngine/Core/Shaders/Terrain/WaterPatchPS.hlsl
+++ MiniEngine/Core/Shaders/Terrain/WaterPatchPS.hlsl
@@ -1,8 +1,16 @@
 #include "TerrainTessellation.hlsli"
 
+float3 ComputeWaterMicrobump(float2 texUV)
+{
+    float2 microtexUV = texUV * g_WaterConstants.z;
+    float2 timeoffset = float2(g_WaterConstants.y * 1.0f, g_WaterConstants.y * 0.7f);
+    float3 BumpA = normalize(2 * g_WaterBumpMap.Sample(SamplerRepeatLinear, microtexUV - timeoffset * 0.2f).rbg - float3(1, -8, 1));
+    float3 BumpB = normalize(2 * g_WaterBumpMap.Sample(SamplerRepeatLinear, microtexUV * 0.5f - timeoffset * 0.05f).rbg - float3(1, -8, 1));
+    return normalize(BumpA + BumpB);
+}
+
 float4 WaterPatchPS(MeshVertex input) : SV_Target
 {
-    //return DebugCracksPattern(input);
     float2 vWorldXZ = input.vWorldXYZ.xz;
 
     const float2 heightUV = worldXZtoHeightUV(vWorldXZ);
@@ -10,29 +18,24 @@ float4 WaterPatchPS(MeshVertex input) : SV_Target
     const float waterDepth = g_WaterConstants.x - terrainYpos;
     const float shallowness = 1 - saturate(waterDepth * 100);
     const float shallowAlpha = 1 - pow(shallowness, 4);
-
-//    const float2 heightUV = worldXZtoHeightUV(vWorldXZ);
-//    const float2 grad = g_CoarseGradientMap.Sample(SamplerRepeatLinear, heightUV).rg;
-//    const float vScale = g_CoarseSampleSpacing.y;
-//    const float3 coarseNormal = normalize(float3(-vScale * grad.x, g_CoarseSampleSpacing.x, -vScale * grad.y));
-//    const float3 normal = coarseNormal;
     const float3 normal = input.vNormal;
 
-    // Texture coords have to be offset by the eye's 2D world position.  Why the 2x???
     const float2 texUV = vWorldXZ + float2(g_TextureWorldOffset.x, -g_TextureWorldOffset.z) * g_tileWorldSize.y;
 
-//    const float4 MatMapSample = g_CoarseMaterialMap.Sample(SamplerRepeatLinear, heightUV);
     float3 TempSpecular = 1;
     float TempSpecularMask = 1;
-//    float3 NormalSample;
-//    float TerrainHeight = (MatMapSample.y * MaterialMapScale) - MaterialMapOffset;
-//    float3 TempDiffuse = TerrainMaterialBlend(MatMapSample.x, TerrainHeight, texUV, TempSpecular, TempSpecularMask, NormalSample);
     float3 TempDiffuse = lerp(float3(0.5, 0.5, 1), float3(1, 1, 1), shallowness);
 
     float3 Tangent = normalize(cross(normal, float3(1, 0, 0)));
     float3 Bitangent = normalize(cross(Tangent, normal));
 
-//    float3 NewNormal = normalize(NormalSample.x * -Tangent + NormalSample.y * Bitangent + NormalSample.z * normal);
+    float3x3 BasisMatrix;
+    BasisMatrix[0] = normal;
+    BasisMatrix[1] = Tangent;
+    BasisMatrix[2] = Bitangent;
+
+    //float3 MicrobumpNormal = ComputeWaterMicrobump(texUV);
+
     float3 NewNormal = normal;
 
     float3 viewDir = normalize(input.vViewDir);
@@ -44,8 +47,5 @@ float4 WaterPatchPS(MeshVertex input) : SV_Target
     {
         LitResult *= input.vNormal.x;
     }
-    //LitResult = NormalSample * 0.5 + 0.5;
     return float4(LitResult, 0.75f * shallowAlpha);
-    //float4 WaterBump = g_WaterBumpMap.SampleLevel(SamplerRepeatLinear, texUV, 0).rbga;
-    //return float4(input.vNormal * 0.5 + 0.5, 1);
 }
